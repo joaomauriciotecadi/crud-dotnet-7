562f295b00bfe4f4fc12164e5dae962f2c047e1d
joaomauriciotecadi <joao.mauricio@tecadi.com.br>
2023-09-21 11:29:20 -0300

    Added Controller and Updated Readme.

--- Models/TodoItem.cs
+++ Models/TodoItem.cs
@@ -4,5 +4,6 @@ public class TodoItem
 {
     public long Id {get; set;}
     public string? Name {get; set;}
-    public bool IsComplete {get; set;}
+    public string? Description {get; set;}
+    public bool IsComplete {get; set;} = false;    
 }
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # crud-dotnet-7
 Crud with dotnet 7.
+
+# Run
+´dotnet run --launch-profile https´
