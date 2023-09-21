9e3947f62d6ffffd295573d8784868cf9cec8246
João Mauricio <111187472+joaomauriciodev@users.noreply.github.com>
2023-09-21 13:23:29 +0000

    Fixed Model TodoItem

--- Models/TodoItem.cs
+++ Models/TodoItem.cs
@@ -3,6 +3,6 @@ namespace crud_dotnet_7.Models;
 public class TodoItem
 {
     public long Id {get; set;}
-    public string? Name {get; set}
-    public bool IsComplete {get; set}
+    public string? Name {get; set;}
+    public bool IsComplete {get; set;}
 }
